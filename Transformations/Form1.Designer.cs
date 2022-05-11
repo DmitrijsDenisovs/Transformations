48dac13c5ea61d32d773cf54cba46bdf5a6c8039
Deniss Dmitrijevs <dod3005@gmail.com>
2022-05-11 11:29:36 +0300

    Add project files.

---
+++ Transformations/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace Transformations
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aTextBox = new System.Windows.Forms.TextBox();
+            this.bTextBox = new System.Windows.Forms.TextBox();
+            this.pTextBox = new System.Windows.Forms.TextBox();
+            this.cTextBox = new System.Windows.Forms.TextBox();
+            this.dTextBox = new System.Windows.Forms.TextBox();
+            this.qTextBox = new System.Windows.Forms.TextBox();
+            this.sTextBox = new System.Windows.Forms.TextBox();
+            this.nTextBox = new System.Windows.Forms.TextBox();
+            this.mTextBox = new System.Windows.Forms.TextBox();
+            this.applyMatrixButton = new System.Windows.Forms.Button();
+            this.finishButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.angleButton = new System.Windows.Forms.Button();
+            this.angleTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // aTextBox
+            // 
+            this.aTextBox.Location = new System.Drawing.Point(741, 16);
+            this.aTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.aTextBox.Name = "aTextBox";
+            this.aTextBox.Size = new System.Drawing.Size(49, 27);
+            this.aTextBox.TabIndex = 0;
+            // 
+            // bTextBox
+            // 
+            this.bTextBox.Location = new System.Drawing.Point(797, 16);
+            this.bTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bTextBox.Name = "bTextBox";
+            this.bTextBox.Size = new System.Drawing.Size(47, 27);
+            this.bTextBox.TabIndex = 1;
+            // 
+            // pTextBox
+            // 
+            this.pTextBox.Location = new System.Drawing.Point(851, 16);
+            this.pTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pTextBox.Name = "pTextBox";
+            this.pTextBox.Size = new System.Drawing.Size(49, 27);
+            this.pTextBox.TabIndex = 2;
+            // 
+            // cTextBox
+            // 
+            this.cTextBox.Location = new System.Drawing.Point(741, 55);
+            this.cTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cTextBox.Name = "cTextBox";
+            this.cTextBox.Size = new System.Drawing.Size(49, 27);
+            this.cTextBox.TabIndex = 3;
+            // 
+            // dTextBox
+            // 
+            this.dTextBox.Location = new System.Drawing.Point(797, 55);
+            this.dTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dTextBox.Name = "dTextBox";
+            this.dTextBox.Size = new System.Drawing.Size(47, 27);
+            this.dTextBox.TabIndex = 4;
+            // 
+            // qTextBox
+            // 
+            this.qTextBox.Location = new System.Drawing.Point(851, 55);
+            this.qTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.qTextBox.Name = "qTextBox";
+            this.qTextBox.Size = new System.Drawing.Size(49, 27);
+            this.qTextBox.TabIndex = 5;
+            // 
+            // sTextBox
+            // 
+            this.sTextBox.Location = new System.Drawing.Point(851, 93);
+            this.sTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.sTextBox.Name = "sTextBox";
+            this.sTextBox.Size = new System.Drawing.Size(49, 27);
+            this.sTextBox.TabIndex = 6;
+            // 
+            // nTextBox
+            // 
+            this.nTextBox.Location = new System.Drawing.Point(797, 93);
+            this.nTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.nTextBox.Name = "nTextBox";
+            this.nTextBox.Size = new System.Drawing.Size(47, 27);
+            this.nTextBox.TabIndex = 7;
+            // 
+            // mTextBox
+            // 
+            this.mTextBox.Location = new System.Drawing.Point(741, 93);
+            this.mTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.mTextBox.Name = "mTextBox";
+            this.mTextBox.Size = new System.Drawing.Size(49, 27);
+            this.mTextBox.TabIndex = 8;
+            // 
+            // applyMatrixButton
+            // 
+            this.applyMatrixButton.Location = new System.Drawing.Point(741, 132);
+            this.applyMatrixButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.applyMatrixButton.Name = "applyMatrixButton";
+            this.applyMatrixButton.Size = new System.Drawing.Size(160, 31);
+            this.applyMatrixButton.TabIndex = 9;
+            this.applyMatrixButton.Text = "Apply matrix";
+            this.applyMatrixButton.UseVisualStyleBackColor = true;
+            this.applyMatrixButton.Click += new System.EventHandler(this.applyMatrixButton_Click);
+            // 
+            // finishButton
+            // 
+            this.finishButton.Location = new System.Drawing.Point(759, 525);
+            this.finishButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.finishButton.Name = "finishButton";
+            this.finishButton.Size = new System.Drawing.Size(142, 31);
+            this.finishButton.TabIndex = 10;
+            this.finishButton.Text = "Finish polygon";
+            this.finishButton.UseVisualStyleBackColor = true;
+            this.finishButton.Click += new System.EventHandler(this.finishButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(759, 564);
+            this.resetButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(142, 31);
+            this.resetButton.TabIndex = 11;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // angleButton
+            // 
+            this.angleButton.Location = new System.Drawing.Point(741, 246);
+            this.angleButton.Name = "angleButton";
+            this.angleButton.Size = new System.Drawing.Size(159, 29);
+            this.angleButton.TabIndex = 12;
+            this.angleButton.Text = "Set rotation matrix";
+            this.angleButton.UseVisualStyleBackColor = true;
+            this.angleButton.Click += new System.EventHandler(this.angleButton_Click);
+            // 
+            // angleTextBox
+            // 
+            this.angleTextBox.Location = new System.Drawing.Point(797, 179);
+            this.angleTextBox.Name = "angleTextBox";
+            this.angleTextBox.Size = new System.Drawing.Size(103, 27);
+            this.angleTextBox.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(741, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Angle";
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(765, 213);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(47, 27);
+            this.xTextBox.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(741, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "X";
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(851, 212);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(47, 27);
+            this.yTextBox.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(827, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.angleTextBox);
+            this.Controls.Add(this.angleButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.finishButton);
+            this.Controls.Add(this.applyMatrixButton);
+            this.Controls.Add(this.mTextBox);
+            this.Controls.Add(this.nTextBox);
+            this.Controls.Add(this.sTextBox);
+            this.Controls.Add(this.qTextBox);
+            this.Controls.Add(this.dTextBox);
+            this.Controls.Add(this.cTextBox);
+            this.Controls.Add(this.pTextBox);
+            this.Controls.Add(this.bTextBox);
+            this.Controls.Add(this.aTextBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox aTextBox;
+        private System.Windows.Forms.TextBox bTextBox;
+        private System.Windows.Forms.TextBox pTextBox;
+        private System.Windows.Forms.TextBox cTextBox;
+        private System.Windows.Forms.TextBox dTextBox;
+        private System.Windows.Forms.TextBox qTextBox;
+        private System.Windows.Forms.TextBox sTextBox;
+        private System.Windows.Forms.TextBox nTextBox;
+        private System.Windows.Forms.TextBox mTextBox;
+        private System.Windows.Forms.Button applyMatrixButton;
+        private System.Windows.Forms.Button finishButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button angleButton;
+        private System.Windows.Forms.TextBox angleTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Transformations/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Transformations
+{
+    public partial class Form1 : Form
+    {
+        private Color[] colors = { Color.Red, Color.Black, Color.Green, Color.Blue, Color.Yellow};
+        private Graphics graphics;
+        private List<Point> polygon = new List<Point>();
+        private int currentColorIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = this.CreateGraphics();
+        }
+
+        private void finishButton_Click(object sender, EventArgs e)
+        {
+            if (polygon.Count > 1)
+            {
+                graphics.DrawLine(new Pen(new SolidBrush(colors[currentColorIndex])), polygon[0], polygon.ElementAt(polygon.Count - 1));
+                ++currentColorIndex;
+                if (currentColorIndex >= colors.Length)
+                {
+                    currentColorIndex = 0;
+                }
+            } 
+            else
+            {
+                MessageBox.Show("Enter more points");
+            }
+            
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            int x = this.PointToClient(Cursor.Position).X;
+            int y = this.PointToClient(Cursor.Position).Y;
+            graphics.DrawRectangle(new Pen(Color.Black, 4), new Rectangle(x, y, 1, 1));
+
+            polygon.Add(this.PointToClient(Cursor.Position));
+            if (polygon.Count > 1)
+            {
+                graphics.DrawLine(new Pen(new SolidBrush(colors[currentColorIndex])), polygon.ElementAt(polygon.Count - 1), polygon.ElementAt(polygon.Count - 2));
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            polygon = new List<Point>();
+            graphics.Clear(Color.LightGray);
+        }
+
+        private void applyMatrixButton_Click(object sender, EventArgs e)
+        {
+            double a, b, c, d, p, q, m, n, s;
+            bool valid = true;
+            if (!Double.TryParse(aTextBox.Text, out a))
+            {
+                if (String.IsNullOrWhiteSpace(aTextBox.Text))
+                {
+                    a = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }    
+            }
+            if (!Double.TryParse(bTextBox.Text, out b))
+            {
+                if (String.IsNullOrWhiteSpace(bTextBox.Text))
+                {
+                    b = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+            if (!Double.TryParse(cTextBox.Text, out c))
+            {
+                if (String.IsNullOrWhiteSpace(cTextBox.Text))
+                {
+                    c = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+            if (!Double.TryParse(dTextBox.Text, out d))
+            {
+                if (String.IsNullOrWhiteSpace(dTextBox.Text))
+                {
+                    d = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+            if (!Double.TryParse(pTextBox.Text, out p))
+            {
+                if (String.IsNullOrWhiteSpace(pTextBox.Text))
+                {
+                    p = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+            if (!Double.TryParse(qTextBox.Text, out q))
+            {
+                if (String.IsNullOrWhiteSpace(pTextBox.Text))
+                {
+                    q = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+            if (!Double.TryParse(mTextBox.Text, out m))
+            {
+                if (String.IsNullOrWhiteSpace(pTextBox.Text))
+                {
+                    m = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+            if (!Double.TryParse(nTextBox.Text, out n))
+            {
+                if (String.IsNullOrWhiteSpace(pTextBox.Text))
+                {
+                    n = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+            if (!Double.TryParse(sTextBox.Text, out s))
+            {
+                if (String.IsNullOrWhiteSpace(pTextBox.Text))
+                {
+                    s = 0.0;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+
+            if (valid)
+            {
+                double[,] transformationMatrix = new double[3,3]{ { a, b, p}, { c, d, q }, { m, n, s } };
+                //double[,] transformationMatrix = new double[3, 3] { { a, c, m }, { b, d, n }, { q, p, s } };
+                ApplyTransformationMatrix(polygon, transformationMatrix);
+            }
+            else
+            {
+                MessageBox.Show("Values must be numbers");
+            }
+        }
+
+        private void ApplyTransformationMatrix(List<Point> polygon, double[,] transformationMatrix)
+        {
+            ++currentColorIndex;
+            if (currentColorIndex >= colors.Length)
+            {
+                currentColorIndex = 0;
+            }
+            bool outOfBounds = false;
+            List<Point> newPolygon = new List<Point>();
+            foreach (Point point in polygon)
+            {
+                double[] pointMatrix = new double[] { point.X, point.Y, 1.0 };
+
+                double[] newPointMatrix = MultiplyMatrixes(pointMatrix, transformationMatrix);
+
+                Point newPoint = new Point((int)newPointMatrix[0], (int)newPointMatrix[1]);
+
+                if (newPoint.X < 0 || newPoint.Y < 0 || newPoint.Y > this.Height || newPoint.X > this.Width)
+                {
+                    outOfBounds = true;
+                }
+                newPolygon.Add(newPoint);
+                if (newPolygon.Count > 1)
+                {
+                    graphics.DrawLine(new Pen(new SolidBrush(colors[currentColorIndex])), newPolygon.ElementAt(newPolygon.Count - 1), newPolygon.ElementAt(newPolygon.Count - 2));
+                }
+            }
+
+            graphics.DrawLine(new Pen(new SolidBrush(colors[currentColorIndex])), newPolygon.ElementAt(0), newPolygon.ElementAt(newPolygon.Count - 1));
+            
+            if (outOfBounds)
+            {
+                MessageBox.Show("Polygons parts are out of bounds");
+            }
+        }
+
+        private double[] MultiplyMatrixes(double[] pointMatrix, double[,] transformationMatrix)
+        {
+            double[] newPointMatrix = new double[3];
+            for (int j = 0; j < 3; j++)
+            {
+                newPointMatrix[j] = 0;
+                for (int k = 0; k < 3; k++)
+                {
+                    newPointMatrix[j] += pointMatrix[k] * transformationMatrix[k, j];
+                }
+            }
+            return newPointMatrix;
+        }
+
+        private void angleButton_Click(object sender, EventArgs e)
+        {
+            double angle;
+            int x, y;
+
+            if (!Double.TryParse(angleTextBox.Text, out angle))
+            {
+                MessageBox.Show("Angle must be a number");
+                return;
+            }
+            if (!String.IsNullOrWhiteSpace(xTextBox.Text) && !String.IsNullOrWhiteSpace(yTextBox.Text))
+            {
+                if (!Int32.TryParse(xTextBox.Text, out x) || !Int32.TryParse(yTextBox.Text, out y))
+                {
+                    MessageBox.Show("Coordinate must be a positive integer");
+                    return;
+                }
+            } else
+            {
+                x = polygon.ElementAt(polygon.Count - 1).X;
+                y = polygon.ElementAt(polygon.Count - 1).Y;
+            }
+
+            double dx = x, dy = y;
+            double angleRadians = angle * Math.PI / 180.0;
+
+            aTextBox.Text = Math.Cos(angleRadians).ToString();
+            bTextBox.Text = Math.Sin(angleRadians).ToString();
+            cTextBox.Text = (-Math.Sin(angleRadians)).ToString();
+            dTextBox.Text = Math.Cos(angleRadians).ToString();
+            mTextBox.Text = (-dx * Math.Cos(angleRadians) + dy * Math.Sin(angleRadians) + dx).ToString();
+            nTextBox.Text = (-dx * Math.Sin(angleRadians) - dy * Math.Cos(angleRadians) + dy).ToString();
+        }
+    }
+}
